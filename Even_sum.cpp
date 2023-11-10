2a4528cd96f6a98a26787bc135392c8d48496b78
RoddyMacrae <roddymacrae@hotmail.com>
2023-11-10 06:10:47 +0000

    Modified to use limit and while loop, limit taken as user input.

--- Even_sum.cpp
+++ Even_sum.cpp
@@ -9,21 +9,22 @@ int main(){
     vector<int> even;
     int Fib = 0;
     int Fib_old = 1;
-
-    int n;
+    
+    int limit;
     cout << "This short macro returns the sum of all the even numbers in the Fibonacci sequence up to the user input (limited up to values no greater than 4 million.)" << endl;
-    cout << "Enter a number: " << endl;
-    cin >> n;
+    cout << "Enter a limit (max value of fib sequence): " << endl;
+    cin >> limit;
 
-    for(int i = 1; i < n; i++){
+    while(Fib < limit){
 
-        Fib = Fib_old + Fib;
-        Fib_old = Fib - Fib_old;
+        Fib = Fib_old;
+        Fib_old = Fib + Fib_old;
 
-        if(Fib%2==0 && Fib <= 4000000){
+        if(Fib%2==0 && Fib < limit){
             cout << "Fibonacci sequence: " << Fib << endl;
             even.push_back(Fib);
         }
+        
     }
 
     cout << "Sum: " << accumulate(even.begin(),even.end(),0) << endl;
